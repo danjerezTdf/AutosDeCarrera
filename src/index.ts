d9daadef086548b7d7b9a36132c9cb3547d5f1e7
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:13:18 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/* Estructuras de Control
+Problema: Autos de Carrera
+• En una prueba, un piloto tiene que completar 4
+vueltas
+• Se necesita un programa que permita ingresar
+por teclado el tiempo de cada vuelta
+• El programa debe retornar el tiempo total y el
+promedio de vuelta */
+
 let vueltaUno: number = Number(prompt(`Ingrese tiempo de vuelta uno`));
 let vueltaDos: number = Number(prompt(`Ingrese tiempo de vuelta dos`));
 let vueltaTres: number = Number(prompt(`Ingrese tiempo de vuelta tres`));
